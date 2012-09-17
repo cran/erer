fa6f214e503c84496f2fa90ffd5f47719bbde0ad
Changyou Sun <csun@cfr.msstate.edu>
2012-09-17 00:00:00 +0000

    version 1.3

--- R/bsStat.R
+++ R/bsStat.R
@@ -1,29 +1,31 @@
-bsStat <- function(y, two = NULL, digits = c(2, 1), ...){
-    digits <- rep(digits, 2)[1:2]
-    cor2 <- round(cor(y), digits = digits[1])
-    corr <- data.frame(name = rownames(cor2), cor2)
-    rownames(corr) <- 1:nrow(corr)
+bsStat <- function(y, two = NULL, digits = c(2, 2), 
+  use = 'complete.obs', na.rm = TRUE, ...)
+{
+  digits <- rep(digits, 2)[1:2]    
+  ff <- data.frame(matrix(data = 0, nrow = ncol(y), ncol = ncol(y) + 6))
+  colnames(ff) <- c("name", "mean", "stde", "mini", "maxi", "obno", colnames(y))
+  ff$name <- colnames(y)
+  for (i in 1:ncol(y)){
+    ff[i, "mean"] <- round(mean(y[, i], na.rm = na.rm), digits=digits[2])
+    ff[i, "stde"] <- round(  sd(y[, i], na.rm = na.rm), digits=digits[2])    
+    ff[i, "mini"] <- round( min(y[, i], na.rm = na.rm), digits=digits[2])    
+    ff[i, "maxi"] <- round( max(y[, i], na.rm = na.rm), digits=digits[2])    
+    ff[i, "obno"] <- length(na.omit(y[, i]))
+    for (j in 1:ncol(y)) {
+      ff[i, j+6] <- round(cor(y[,i], y[,j], use = use), digits=digits[1])
+    }
+  }
+  fstat <- ff[, 1:6]
+  corr <- ff[, -c(2:6)]
 
-    for ( i in (1:(ncol(y)-1) + 2) ) {corr[1:(i-2), i] <- ""}
-    fstat <- data.frame(matrix(data = 0, nrow = ncol(y), ncol = 6))
-    colnames(fstat) <- c("name", "mean", "stde", "mini", "maxi", "obno")
-    fstat$name <- colnames(y)
-    for (i in 1:ncol(y)){
-        fstat[i, "mean"] <- round(mean(y[, i]), digits=digits[2])
-        fstat[i, "stde"] <- round(sd(y[,i])   , digits=digits[2])    
-        fstat[i, "mini"] <- round(min(y[,i])  , digits=digits[2])    
-        fstat[i, "maxi"] <- round(max(y[,i])  , digits=digits[2])    
-        fstat[i, "obno"] <- length(y[,i])
-    }
 
-    if (is.null(two)) {
-        if (ncol(y) < 11) { two <- FALSE
-        } else { two <- TRUE }
-    }
-    if (two) {
-        result <- list(corr=corr, fstat=fstat)
-    } else {
-        result <- merge(x=corr, y=fstat, by="name", sort = FALSE)
-    }
-    return(result)
+  if (is.null(two)) {
+      if (ncol(y) < 11) { two <- FALSE } else { two <- TRUE }
+  }
+  if (two) {
+      result <- list(fstat = fstat, corr = corr)
+  } else {
+      result <- ff
+  }
+  return(result)
 }
